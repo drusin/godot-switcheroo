51402e523b94cc2a056333dce4f7f9f8a744b91b
drusin <dawidrusin90@gmail.com>
2023-04-20 15:46:52 +0200

    use low precessor usage mode

--- src/main.gd
+++ src/main.gd
@@ -3,8 +3,12 @@ extends Control
 @onready var Bottom := $Content/Bottom
 
 
+func _ready() -> void:
+	OS.low_processor_usage_mode = true
+
+
 func _on_tabs_tab_changed(tab: int) -> void:
 	for child in Bottom.get_children():
 		if not "index" in child:
-			return
+			continue
 		child.visible = tab == child.index
