4b57dfec4a026b4545f866ab6b3395ee3e7ebb4b
drusin <dawidrusin90@gmail.com>
2023-05-03 00:15:26 +0200

    Custom install dialog done

--- src/installations/import_window.gd
+++ src/installations/import_window.gd
@@ -1,16 +1,21 @@
 extends ConfirmationDialog
+class_name CustomVersionDialog
+
+signal version_created(version: INSTALLATIONS.GodotVersion)
+signal version_edited(version: EditedGodotVersion)
 
 @export var path := "":
 	set(new_val):
 		path = new_val
 		if PathEdit:
 			PathEdit.text = new_val
+			PathEdit.caret_column = new_val.length()
 
 @export var version := "":
 	set(new_val):
 		version = new_val
-		if NameEdit:
-			NameEdit.text = new_val
+		if VersionEdit:
+			VersionEdit.text = new_val
 
 @export var custom_name := "":
 	set(new_val):
@@ -18,6 +23,53 @@ extends ConfirmationDialog
 		if NameEdit:
 			NameEdit.text = new_val
 
-@onready var PathEdit: LineEdit = $Content/Path/PathEdit
+@onready var PathEdit: LineEdit = $Content/Path/PathContent/PathEdit
 @onready var VersionEdit: LineEdit = $Content/Version/VersionEdit
 @onready var NameEdit: LineEdit = $Content/CustomName/NameEdit
+@onready var PathDialog: FileDialog = $PathDialog
+@onready var MandatoryAlert: AcceptDialog = $MandatoryAlert
+
+var old_name := ""
+
+
+func custom_popup(current_name := "") -> void:
+	old_name = current_name
+	popup()
+
+
+func _on_browse_button_pressed() -> void:
+	PathDialog.popup()
+
+
+func _on_path_dialog_file_selected(new_path: String) -> void:
+	path = new_path
+
+
+func _on_confirmed() -> void:
+	path = PathEdit.text
+	version = VersionEdit.text
+	custom_name = NameEdit.text
+
+	if path == "" or version == "" or custom_name == "":
+		MandatoryAlert.popup()
+		return
+
+	var godot_version := INSTALLATIONS.GodotVersion.new()
+	godot_version.installation_path = path
+	godot_version.version = version
+	godot_version.custom_name = custom_name
+	godot_version.is_custom = true
+
+	if old_name == "":
+		emit_signal("version_created", godot_version)
+	else:
+		var edited_version := EditedGodotVersion.new()
+		edited_version.old_name = old_name
+		edited_version.godot_version = godot_version
+		emit_signal("version_edited", edited_version)
+	hide()
+
+
+class EditedGodotVersion extends RefCounted:
+	var old_name := ""
+	var godot_version: INSTALLATIONS.GodotVersion
--- src/installations/installations_tab.gd
+++ src/installations/installations_tab.gd
@@ -2,12 +2,12 @@ extends Control
 
 @export var index := 1
 
-@onready var ImportDialog: FileDialog = $ImportDialog
+@onready var CustomVersionDialog: CustomVersionDialog = $CustomVersionDialog
 
 
 func _on_import_pressed() -> void:
-	ImportDialog.popup()
+	CustomVersionDialog.custom_popup()
 
 
-func _on_import_dialog_file_selected(path):
+func _on_custom_version_dialog_version_created(version: INSTALLATIONS.GodotVersion) -> void:
 	pass # Replace with function body.
