21175402b93b70ffa9c31eaad99e509865bcefba
drusin <dawidrusin90@gmail.com>
2023-05-06 12:26:27 +0200

    fixed overlapping installation widgets

--- src/installations/installation_line.gd
+++ src/installations/installation_line.gd
@@ -3,6 +3,8 @@ class_name InstallationLine
 
 signal favourite_changed(favourite: bool)
 
+const CUSTOM_INSTALL_WIDGET_HEIGHT := 98
+
 @export var is_favourite := false
 @export var custom_name := ""
 @export var version := "4.0.3 RC 1"
@@ -18,15 +20,12 @@ signal favourite_changed(favourite: bool)
 
 
 func _ready():
-	if custom_name:
-		CustomNameLabel.text = custom_name
-		CustomNameLabel.visible = true
 	select_button_path = get_node("SelectButton").get_path()
 	FavouriteButton.button_pressed = is_favourite
 	PseudoIcon.text = version.substr(0, 3)
+	if custom_name:
+		CustomNameLabel.text = custom_name
+		CustomNameLabel.visible = true
+		custom_minimum_size.y = CUSTOM_INSTALL_WIDGET_HEIGHT
 	VersionLabel.text = version
 	PathLabel.text = path
-
-
-func _on_visibility_changed():
-	(func (): size.y = Widgets.size.y).call_deferred()
