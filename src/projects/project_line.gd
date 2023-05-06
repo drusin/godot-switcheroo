1b511ca84af245034190cd7598b0e71b7d3382bc
drusin <dawidrusin90@gmail.com>
2023-05-06 11:53:55 +0200

    simplify code

--- src/projects/project_line.gd
+++ src/projects/project_line.gd
@@ -3,29 +3,10 @@ class_name ProjectLine
 
 signal favourite_changed(favourite: bool)
 
-@export var is_favourite := false:
-	set(newVal):
-		is_favourite = newVal
-		if FavouriteButton:
-			FavouriteButton.button_pressed = newVal
-
-@export_global_file var project_icon: String:
-	set(newVal):
-		project_icon = newVal
-		if Icon:
-			Icon.texture = _create_external_texture(newVal)
-
-@export var project_name := "Project name":
-	set(newVal):
-		project_name = newVal
-		if NameLabel:
-			NameLabel.text = newVal
-
-@export var project_path := "project/path":
-	set(newVal):
-		project_path = newVal
-		if PathLabel:
-			PathLabel.text = newVal
+@export var is_favourite := false
+@export_global_file var project_icon: String
+@export var project_name := "Project name"
+@export var project_path := "project/path"
 
 @onready var FavouriteButton: CheckButton = $Widgets/Favourite
 @onready var Icon: TextureRect = $Widgets/Icon
