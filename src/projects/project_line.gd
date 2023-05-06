36b099e82f4ade973d5d0647e390070cb38010cd
drusin <dawidrusin90@gmail.com>
2023-05-06 14:47:55 +0200

    wip setting version

--- src/projects/project_line.gd
+++ src/projects/project_line.gd
@@ -10,8 +10,10 @@ signal favourite_changed(favourite: bool)
 
 @onready var FavouriteButton: CheckButton = $Widgets/Favourite
 @onready var Icon: TextureRect = $Widgets/Icon
-@onready var NameLabel: Label = $Widgets/Content/Name
-@onready var PathLabel: Label = $Widgets/Content/Path
+@onready var NameLabel: Label = $Widgets/Content/NameContainer/Name
+@onready var PathLabel: RichTextLabel = $Widgets/Content/NameContainer/PathCenterContainer/Path
+@onready var SetVersionButton: Button = $Widgets/SetVersionButton
+@onready var VersionSelectMenu: PopupMenu = $VersionSelectMenu
 
 
 func _ready() -> void:
@@ -19,10 +21,15 @@ func _ready() -> void:
 	FavouriteButton.button_pressed = is_favourite
 	Icon.texture = _create_external_texture(project_icon)
 	NameLabel.text = project_name
-	PathLabel.text = project_path
+	PathLabel.text = "[i]" + project_path + "[/i]"
 
 
 func _create_external_texture(texture_path: String) -> ImageTexture:
 	var image = Image.new()
 	image.load(texture_path)
 	return ImageTexture.create_from_image(image)
+
+
+func _on_set_version_button_pressed() -> void:
+	VersionSelectMenu.show()
+	VersionSelectMenu.position = SetVersionButton.position
